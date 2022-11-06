f0e524dee5cfe782311776715e31658b45277a6d
Serkan Koç <serkan.koc@marun.edu.tr>
2022-11-06 14:35:04 +0300

    Section 21: Hibernate CRUD Features: Create, Read, Update and Delete

--- src/com/luv2code/hibernate/demo/entity/Student.java
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -1,21 +1,22 @@
 package com.luv2code.hibernate.demo.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student {
     @Id
-    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
-    @Column(name = "firstName")
+
+    @Column(name="first_name")
     private String firstName;
-    @Column(name = "lastName")
+
+    @Column(name="last_name")
     private String lastName;
-    @Column(name = "email")
+
+    @Column(name="email")
     private String email;
 
     public Student() {
